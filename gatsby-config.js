e066337b7419b4ad3e83a462a4b4df2a1785f117
Bernhard Gschwantner <bernhard@unserwein.at>
2018-09-19 16:38:19 +0200

    first prototype of index page header using gatsby-starter-default

---
+++ gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {},
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `${__dirname}/src/assets/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Bernhard Gschwantner',
+        short_name: 'gschwantner',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    'gatsby-plugin-offline',
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import { Flex, Box, Image, Heading, Text } from 'rebass';
+import Layout from '../components/layout';
+
+const IndexPage = ({
+  data: {
+    image: {
+      childImageSharp: { fluid },
+    },
+  },
+}) => (
+  <Layout>
+    <Box
+      as="header"
+      css={`
+        background: #c90057 linear-gradient(to bottom left, #c90057, #ea5971)
+          fixed;
+      `}
+    >
+      <Flex
+        alignItems="center"
+        justifyContent="flex-end"
+        color="white"
+        flexDirection={['column', 'row']}
+        css={`
+          @media screen and (min-width: 64em) and (max-height: 50em) {
+            min-height: 100vh;
+          }
+        `}
+      >
+        <Box
+          width={[1, 2 / 3, 1 / 2]}
+          fontSize={[3, 4]}
+          pl={[2, 3]}
+          pr={[2, 0]}
+          py={5}
+          css={`
+            max-width: 500px;
+          `}
+        >
+          <Box mb={[3, 4]}>
+            <Text mb={1}>Hi, my name is</Text>
+            <Heading as="h1" fontSize={[6, 7, 8, 96]}>
+              Bernhard{' '}
+              <Text fontSize={[5, 6, 6, 8]} as="small" css="display:block;">
+                Gschwantner.
+              </Text>
+            </Heading>
+          </Box>
+
+          <Text mb={3} css="max-width: 16em;">
+            I'm a long-time software developer, founder and tech enthusiast.
+          </Text>
+          <Box as={Link} color="white" to="/about">
+            More about me
+          </Box>
+        </Box>
+
+        <Box width={[1, 1 / 3, 1 / 2]} css="align-self: flex-end;">
+          <Image as={Img} fluid={fluid} />
+        </Box>
+      </Flex>
+    </Box>
+  </Layout>
+);
+
+export default IndexPage;
+
+export const query = graphql`
+  query {
+    image: file(name: { eq: "bernhard" }) {
+      childImageSharp {
+        fluid(maxWidth: 1280) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`;
