41ea5abceb066cf87a842172668187b95ac78be6
Bernhard Gschwantner <bernhard@unserwein.at>
2018-09-20 10:59:10 +0200

    created a Header component with an animated gradient

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,21 @@ import { Link, StaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
 import { Flex, Box, Image, Heading, Text } from 'rebass';
 import Layout from '../components/layout';
+import { keyframes, css } from 'styled-components';
+
+const moveBackground = keyframes`
+  0% {
+		background-position: 100% 50%;
+	}
+
+	50% {
+		background-position: 0% 50%;
+  }
+
+  100% {
+		background-position: 100% 50%;
+	}
+`;
 
 const Container = props => (
   <Box
@@ -18,6 +33,7 @@ const Container = props => (
 
 const Project = props => (
   <Flex
+    as="section"
     alignItems="center"
     bg="lightblue"
     p={4}
@@ -31,82 +47,87 @@ const Project = props => (
   />
 );
 
-const Post = props => <Box pb={4} {...props} />;
+const gradient = ['#660022', '#c90057', '#ea5971', '#ff8c00'];
+
+const Header = ({ css: additionalCss = '', ...props }) => (
+  <Flex
+    as="header"
+    css={css`
+      background-image: linear-gradient(to bottom left, ${gradient.join(',')});
+      background-color: #c90057;
+      background-attachment: fixed;
+      background-size: 400% 100%;
+      background-position: 50% 50%;
+      animation: ${moveBackground} 15s ease-in-out infinite;
+      ${additionalCss};
+    `}
+    {...props}
+  />
+);
+
+const Post = props => <Box pb={4} as="section" {...props} />;
 
 const IndexPage = () => (
   <Layout>
-    <Box
-      as="header"
+    <Header
+      alignItems="center"
+      justifyContent="flex-end"
+      color="white"
+      flexDirection={['column', 'row']}
       css={`
-        background: #c90057 linear-gradient(to bottom left, #c90057, #ea5971)
-          fixed;
+        @media screen and (min-width: 64em) and (max-height: 50em) {
+          min-height: 100vh;
+        }
       `}
     >
-      <Flex
-        alignItems="center"
-        justifyContent="flex-end"
-        color="white"
-        flexDirection={['column', 'row']}
+      <Box
+        width={[1, 2 / 3, 1 / 2]}
+        fontSize={[3, 4]}
+        pl={[2, 3]}
+        pr={[2, 0]}
+        py={5}
         css={`
-          @media screen and (min-width: 64em) and (max-height: 50em) {
-            min-height: 100vh;
-          }
+          max-width: 500px;
         `}
       >
-        <Box
-          width={[1, 2 / 3, 1 / 2]}
-          fontSize={[3, 4]}
-          pl={[2, 3]}
-          pr={[2, 0]}
-          py={5}
-          css={`
-            max-width: 500px;
-          `}
-        >
-          <Box mb={[3, 4]}>
-            <Text mb={1}>Hi, my name is</Text>
-            <Heading as="h1" fontSize={[6, 7, 8, 96]}>
-              Bernhard{' '}
-              <Text fontSize={[5, 6, 6, 8]} as="small" css="display:block;">
-                Gschwantner.
-              </Text>
-            </Heading>
-          </Box>
-
-          <Text mb={3} css="max-width: 16em;">
-            I'm a long-time software developer, founder and tech enthusiast.
-          </Text>
-          <Box as={Link} color="white" to="/about">
-            More about me
-          </Box>
+        <Box mb={[3, 4]}>
+          <Text mb={1}>Hi, my name is</Text>
+          <Heading as="h1" fontSize={[6, 7, 8, 96]}>
+            Bernhard{' '}
+            <Text fontSize={[5, 6, 6, 8]} as="small" css="display:block;">
+              Gschwantner.
+            </Text>
+          </Heading>
         </Box>
 
-        <Box width={[1, 1 / 3, 1 / 2]} css="align-self: flex-end;">
-          <StaticQuery
-            query={graphql`
-              query BernhardImage {
-                image: file(name: { eq: "bernhard" }) {
-                  childImageSharp {
-                    fluid(maxWidth: 1280) {
-                      ...GatsbyImageSharpFluid_withWebp
-                    }
+        <Text mb={3} css="max-width: 16em;">
+          I'm a long-time software developer, founder and tech enthusiast.
+        </Text>
+        <Box as={Link} color="white" to="/about">
+          More about me
+        </Box>
+      </Box>
+      <Box width={[1, 1 / 3, 1 / 2]} css="align-self: flex-end;">
+        <StaticQuery
+          query={graphql`
+            query BernhardImage {
+              image: file(name: { eq: "bernhard" }) {
+                childImageSharp {
+                  fluid(maxWidth: 1280) {
+                    ...GatsbyImageSharpFluid_withWebp
                   }
                 }
               }
-            `}
-            render={data => (
-              <Image
-                as={Img}
-                fluid={data.image.childImageSharp.fluid}
-                critical
-              />
-            )}
-          />
-        </Box>
-      </Flex>
-    </Box>
+            }
+          `}
+          render={data => (
+            <Image as={Img} fluid={data.image.childImageSharp.fluid} critical />
+          )}
+        />
+      </Box>
+    </Header>
 
-    <Container>
+    <Container as="section">
       <Heading mb={4}>Case Studies</Heading>
       <Flex mr={-2} flexWrap="wrap">
         <Project bg="lightblue">First project</Project>
@@ -121,7 +142,7 @@ const IndexPage = () => (
       <Link to="/projects">All Projects</Link>
     </Container>
 
-    <Container>
+    <Container as="section">
       <Heading mb={4}>Recent Posts</Heading>
       <Post>First Post</Post>
       <Post>Second Post</Post>
