0efe645a5ae7e4563c86e2c48558b2f715f55de0
Bernhard Gschwantner <bernhard@unserwein.at>
2018-09-22 16:07:28 +0200

    moved the times for the colors around a bit

--- src/helpers/getTimeColor.js
+++ src/helpers/getTimeColor.js
@@ -1,9 +1,9 @@
 import { mix } from 'polished';
 
 const timeColors = [
-  { time: 6, color: '#e250db' },
-  { time: 12, color: '#2196f3' },
-  { time: 21, color: '#ea5971' },
+  { time: 8, color: '#2196f3' },
+  { time: 12, color: '#e250db' },
+  { time: 16, color: '#ea5971' },
   { time: 24, color: '#6046bd' },
 ];
 
@@ -13,6 +13,9 @@ export default function getTimeColor(date) {
   const previousIndex = nextIndex === 0 ? timeColors.length - 1 : nextIndex - 1;
   const next = timeColors[nextIndex];
   const previous = timeColors[previousIndex];
-  const mixFactor = (currentTime - previous.time) / (next.time - previous.time);
+  const mixFactor = Math.max(
+    0,
+    Math.min(1, (currentTime - previous.time) / (next.time - previous.time))
+  );
   return mix(mixFactor, next.color, previous.color);
 }
