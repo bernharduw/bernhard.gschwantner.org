9110103d58da9f769752fc05032fc31a75959b3b
Bernhard Gschwantner <bernhard@unserwein.at>
2018-09-23 11:15:19 +0200

    created a TimeColor component instead of the getTimeColor function

--- src/components/TimeColor.js
+++ src/components/TimeColor.js
@@ -1,13 +1,14 @@
+import React from 'react';
 import { mix } from 'polished';
 
 const timeColors = [
-  { time: 8, color: '#2196f3' },
+  { time: 4, color: '#2196f3' },
   { time: 12, color: '#e250db' },
   { time: 16, color: '#ea5971' },
   { time: 24, color: '#6046bd' },
 ];
 
-export default function getTimeColor(date) {
+function getTimeColor(date) {
   const currentTime = date.getHours() + date.getMinutes() / 60;
   const nextIndex = timeColors.findIndex(({ time }) => time > currentTime);
   const previousIndex = nextIndex === 0 ? timeColors.length - 1 : nextIndex - 1;
@@ -18,4 +19,27 @@ export default function getTimeColor(date) {
     Math.min(1, (currentTime - previous.time) / (next.time - previous.time))
   );
   return mix(mixFactor, next.color, previous.color);
+}
+
+export default class TimeColor extends React.PureComponent {
+  state = { color: getTimeColor(new Date()) };
+
+  interval = undefined;
+
+  componentDidMount() {
+    // Update the color every 10 minutes.
+    this.interval = setInterval(
+      () => this.setState({ color: getTimeColor(new Date()) }),
+      600000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    console.log(this.state.color, new Date());
+    return this.props.children(this.state.color);
+  }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,7 @@ import Layout from '../components/layout';
 import Header from '../components/MainHeader';
 import Container from '../components/Container';
 import SemiContainer from '../components/SemiContainer';
-import getTimeColor from '../helpers/getTimeColor';
-
-const color = getTimeColor(new Date());
-console.log(color);
+import TimeColor from '../components/TimeColor';
 
 const Project = props => (
   <Flex
@@ -31,65 +28,69 @@ const Post = props => <Box pb={4} as="section" {...props} />;
 
 const IndexPage = () => (
   <Layout>
-    <Header
-      // animate
-      color={color}
-      css={`
-        @media screen and (min-width: 64em) and (max-height: 50em) {
-          min-height: 100vh;
-        }
-      `}
-    >
-      <SemiContainer width={[1, 2 / 3, 1 / 2]} fontSize={[3, 4]} py={5}>
-        <Text mb={1}>Hi, my name is</Text>
-        <Heading as="h1" fontSize={[6, 7, 8, 96]} mb={[3, 4]}>
-          Bernhard{' '}
-          <Text fontSize={[5, 6, 6, 8]} as="small" css="display:block;">
-            Gschwantner.
-          </Text>
-        </Heading>
-
-        <Text mb={4} css="max-width: 16em;">
-          I'm a long-time software developer, founder and tech enthusiast.
-        </Text>
-        <a href="#about">
-          <Button
-            bg="white"
-            color={color}
-            fontWeight="normal"
-            border="2px solid #fff"
-            css={`
-              transition: color 0.3s ease-out, background 0.3s ease-out;
-              &:hover {
-                background-color: transparent;
-                color: #fff;
-              }
-            `}
-          >
-            More about me
-          </Button>
-        </a>
-      </SemiContainer>
-
-      <Box width={[1, 1 / 3, 1 / 2]} css="align-self: flex-end;">
-        <StaticQuery
-          query={graphql`
-            query BernhardImage {
-              image: file(name: { eq: "bernhard" }) {
-                childImageSharp {
-                  fluid(maxWidth: 1280) {
-                    ...GatsbyImageSharpFluid_withWebp
-                  }
-                }
-              }
+    <TimeColor>
+      {color => (
+        <Header
+          // animate
+          color={color}
+          css={`
+            @media screen and (min-width: 64em) and (max-height: 50em) {
+              min-height: 100vh;
             }
           `}
-          render={data => (
-            <Image fluid={data.image.childImageSharp.fluid} critical />
-          )}
-        />
-      </Box>
-    </Header>
+        >
+          <SemiContainer width={[1, 2 / 3, 1 / 2]} fontSize={[3, 4]} py={5}>
+            <Text mb={1}>Hi, my name is</Text>
+            <Heading as="h1" fontSize={[6, 7, 8, 96]} mb={[3, 4]}>
+              Bernhard{' '}
+              <Text fontSize={[5, 6, 6, 8]} as="small" css="display:block;">
+                Gschwantner.
+              </Text>
+            </Heading>
+
+            <Text mb={4} css="max-width: 16em;">
+              I'm a long-time software developer, founder and tech enthusiast.
+            </Text>
+            <a href="#about">
+              <Button
+                bg="white"
+                color={color}
+                fontWeight="normal"
+                border="2px solid #fff"
+                css={`
+                  transition: color 0.3s ease-out, background 0.3s ease-out;
+                  &:hover {
+                    background-color: transparent;
+                    color: #fff;
+                  }
+                `}
+              >
+                More about me
+              </Button>
+            </a>
+          </SemiContainer>
+
+          <Box width={[1, 1 / 3, 1 / 2]} css="align-self: flex-end;">
+            <StaticQuery
+              query={graphql`
+                query {
+                  image: file(name: { eq: "bernhard" }) {
+                    childImageSharp {
+                      fluid(maxWidth: 1280) {
+                        ...GatsbyImageSharpFluid_withWebp
+                      }
+                    }
+                  }
+                }
+              `}
+              render={data => (
+                <Image fluid={data.image.childImageSharp.fluid} critical />
+              )}
+            />
+          </Box>
+        </Header>
+      )}
+    </TimeColor>
 
     <Container as="section" id="about">
       <Heading mb={4}>About me</Heading>
@@ -106,7 +107,9 @@ const IndexPage = () => (
       <Container>
         <Heading mb={4}>Recent work</Heading>
         <Flex mr={-2} flexWrap="wrap">
-          <Project bg="lightblue">First project</Project>
+          <Project bg="lightblue">
+            <Link to="./projects/landhaus-altlengbach">First Project</Link>
+          </Project>
           <Project bg="lightgreen">Second project</Project>
           <Project bg="yellow">Third project</Project>
           <Project bg="orange">Fourth project</Project>
