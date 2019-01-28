33bad01cfeab950d3fcf54d8fbc6b66c9c3c9045
Bernhard Gschwantner <bernhard@unserwein.at>
2019-01-28 17:27:54 +0100

    temporarily added gatsby-plugin-remove-serviceworker

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,5 +29,6 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
+    'gatsby-plugin-remove-serviceworker',
   ],
 };
