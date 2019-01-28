cf1f30a7cdd105174f03a295d6e9ee30ee10038a
Bernhard Gschwantner <bernhard@unserwein.at>
2019-01-28 17:09:17 +0100

    adde contact data to the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import { Box, Text } from 'rebass';
 import Container from '../components/Container';
-import { H2 } from '../components/Text';
+import { H2, P } from '../components/Text';
+import { Box } from 'rebass';
+import Link from './Link';
 
 export default function Footer() {
   return (
-    <Box bg="#555" color="#fff" as="footer">
+    <Box bg="#555" color="#fff" as="footer" pb={7}>
       <Container py={4}>
-        <H2>Footer</H2>
-        <Text>First nav item</Text>
-        <Text>Second nav item</Text>
-        <Text>Third nav item</Text>
+        <H2>Contact</H2>
+        <P>
+          Bernhard Gschwantner
+          <br />
+          Neustiftgasse 17/24
+          <br />
+          1070 Wien
+        </P>
+        <P>
+          Email:{' '}
+          <Link href="mailto:bernhard@gschwantner.org">
+            bernhard@gschwantner.org
+          </Link>
+          <br />
+          Mobile: <Link href="tel:+4369919713296">+43 699 19713296</Link>
+        </P>
       </Container>
     </Box>
   );
