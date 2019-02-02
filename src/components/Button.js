ed2190e658eae0ec27fc80feac1b600d57e4e247
Bernhard Gschwantner <bernhard@unserwein.at>
2019-02-02 12:30:05 +0100

    changed the Button to use css

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,33 +1,36 @@
 import React from 'react';
-import styled from 'styled-components';
 import Link from './Link';
 
-const StyledLink = styled(Link)`
-  transition: color 0.3s ease-out, background 0.3s ease-out;
-  border: 2px solid ${props => props.color};
-  border-radius: 50em;
-  font-weight: normal;
-  :hover {
-    background-color: ${props => props.color};
-    color: ${props => props.hoverColor};
-    text-decoration: none;
-  }
-  :focus {
-    outline: none;
-    box-shadow: 0 0 0 2px;
-  }
-  :disabled {
-    opacity: 0.65;
-    :hover {
-      background-color: transparent;
-      color: currentColor;
-    }
-  }
-  > svg {
-    vertical-align: middle;
-  }
-`;
-
-export default function Button(props) {
-  return <StyledLink p={3} {...props} />;
+export default function Button({ color, hoverColor, ...rest }) {
+  return (
+    <Link
+      p={3}
+      css={`
+        transition: color 0.3s ease-out, background 0.3s ease-out;
+        border: 2px solid ${color};
+        border-radius: 50em;
+        font-weight: normal;
+        :hover {
+          background-color: ${color};
+          color: ${hoverColor};
+          text-decoration: none;
+        }
+        :focus {
+          outline: none;
+          box-shadow: 0 0 0 2px;
+        }
+        :disabled {
+          opacity: 0.65;
+          :hover {
+            background-color: transparent;
+            color: currentColor;
+          }
+        }
+        > svg {
+          vertical-align: middle;
+        }
+      `}
+      {...rest}
+    />
+  );
 }
